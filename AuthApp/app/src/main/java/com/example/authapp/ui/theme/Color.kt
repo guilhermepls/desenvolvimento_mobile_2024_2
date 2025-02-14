4d1c20b3173a68633bc17cb59710926ecce0e95b
guilhermepls <guipinheiro17@alu.ufc.br>
2025-02-14 16:39:15 -0300

    Adicionando: Nova atividade AuthApp

---
+++ AuthApp/app/src/main/java/com/example/authapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.authapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue80 = Color(0xFFBBDEFB)
+val BlueGrey80 = Color(0xFF90A4AE)
+val LightBlue80 = Color(0xFFB3E5FC)
+
+val Blue40 = Color(0xFF42A5F5)
+val BlueGrey40 = Color(0xFF78909C)
+val LightBlue40 = Color(0xFF81D4FA)
